84316b20dd3e45d389d8d3f24672ff59ceedc6cc
Christoffer <Christoffer@192.168.1.100>
2013-01-15 15:40:34 +0200

    Initial commit

---
+++ Javalabra 2013/src/org/fridlund/javalabra/game/Game.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fridlund.javalabra.game;
+
+/**
+ *
+ * @author Christoffer
+ */
+public class Game {
+    
+}
