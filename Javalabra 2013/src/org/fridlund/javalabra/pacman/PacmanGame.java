c8d0029b1136ac0b7c19b899da8bceac5d39b70b
Christoffer <christoffer.fridlund@helsinki.fi>
2013-01-15 21:07:12 +0200

    Added a game hub, from where you can start the games... For now I'll focus on the Pacman clone...

--- Javalabra 2013/src/org/fridlund/javalabra/pacman/PacmanGame.java
+++ Javalabra 2013/src/org/fridlund/javalabra/pacman/PacmanGame.java
@@ -4,6 +4,10 @@
  */
 package org.fridlund.javalabra.pacman;
 
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 import org.fridlund.javalabra.game.Game;
 
@@ -12,6 +16,9 @@ import org.fridlund.javalabra.game.Game;
  * @author Christoffer
  */
 public class PacmanGame extends Game {
+    
+    private BufferedImage backBuffer = new BufferedImage(this.WIDTH, this.HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) backBuffer.getRaster().getDataBuffer()).getData();
 
     public PacmanGame(JFrame parent) {
         super(parent, "Pacman");
@@ -23,5 +30,12 @@ public class PacmanGame extends Game {
 
     @Override
     public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(2);
+            return;
+        }
+        
+        Graphics g = bs.getDrawGraphics();
     }
 }
